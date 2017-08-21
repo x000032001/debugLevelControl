4d29fbb4697a7dc71b9414294268883b6de297a3
葉家郡 <s000032001@gmail.com>
2017-08-21 17:18:18 +0800

    Update Logger.h

--- Logger.h
+++ Logger.h
@@ -2,26 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <syslog.h>
 
 #ifndef DEBUG_CTRL_H
 #define DEBUG_CTRL_H
-#define DEBUG_CTRL_CONTINUE
 
-	#define DEBUG 1
-	#define INFO  2
-	#define WARN  3
-	#define ERROR 4
+        #define DEBUG 1
+        #define INFO  2
+        #define WARN  3
+        #define ERROR 4
+        #define CRIT  5
 
-#endif
-
-#ifndef AUTHER_x000032001
-
-	extern int xxxDebugLevel __attribute__((unused));
-
-#endif
-
-#ifdef DEBUG_CTRL_CONTINUE
-
+        extern int xxxDebugLevel __attribute__((unused));
 
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
@@ -33,17 +25,42 @@
 #define RESET "\x1B[0m"
 
     #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-	#define SET_LOG_LEVEL(x) (xxxDebugLevel=(x))
 
-	#define log(inputLV,format,...)			\
-			do { if((inputLV)>=xxxDebugLevel) {		\
-                if(inputLV==DEBUG) fprintf(stderr,CYN); \
-                if(inputLV==INFO) fprintf(stderr,WHT); \
-                if(inputLV==WARN) fprintf(stderr,YEL); \
+    #define SET_DEBUG_LEVEL(x)  (xxxDebugLevel=(x))
+    #define GET_DEBUG_LEVEL()   (xxxDebugLevel)
+
+#ifndef PRODUCTION
+/* colorful message and print to stderr */
+        #define open_slog(x)    ((void)0)
+        #define slogf(inputLV,format,...)                       \
+                do { if((inputLV)>=xxxDebugLevel) {             \
+                if(inputLV==INFO) fprintf(stderr,WHT);  \
+                if(inputLV==WARN) fprintf(stderr,YEL);  \
                 if(inputLV==ERROR) fprintf(stderr,RED); \
-                fprintf(stderr,"[%5d]%5s| %.8s:%3d %.10s()# " format RESET,getpid(),#inputLV,__FILENAME__,__LINE__,__func__,##__VA_ARGS__);	\
-			} \
-            if((inputLV)>=ERROR) { exit(__LINE__); } \
+                if(inputLV==CRIT) fprintf(stderr,CYN);  \
+                fprintf(stderr,"[%5d]%5s|%.12s:%4d %.10s()# " format RESET,getpid(),#inputLV,__FILENAME__,__LINE__,__func__,##__VA_ARGS__);                                        \
+                if((inputLV)>=CRIT) { exit(__LINE__); } }               \
             } while(0)
+#else
+/* in production, send to syslog */
+        #define open_slog(x)    openlog(x, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1)
 
+        #define slogf(inputLV,format,...)                       \
+                do { if((inputLV)>=xxxDebugLevel) {             \
+                if(inputLV==DEBUG)                              \
+                        syslog(LOG_DEBUG,"%5s| %.8s:%3d %.10s()# " format,#inputLV,__FILENAME__,__LINE__,__func__,##__VA_ARGS__);                                           \
+                if(inputLV==INFO)                               \
+                        syslog(LOG_INFO,"%5s| %.8s:%3d %.10s()# " format,#inputLV,__FILENAME__,__LINE__,__func__,##__VA_ARGS__);                                            \
+                if(inputLV==WARN)                               \
+                        syslog(LOG_WARNING,"%5s| %.8s:%3d %.10s()# " format,#inputLV,__FILENAME__,__LINE__,__func__,##__VA_ARGS__);                                         \
+                if(inputLV==ERROR)                              \
+                        syslog(LOG_ERR,"%5s| %.8s:%3d %.10s()# " format,#inputLV,__FILENAME__,__LINE__,__func__,##__VA_ARGS__);                                     \
+                if(inputLV==CRIT)                               \
+                        syslog(LOG_CRIT,"%5s| %.8s:%3d %.10s()# " format,#inputLV,__FILENAME__,__LINE__,__func__,##__VA_ARGS__);                                            \
+                if((inputLV)>=CRIT) { exit(__LINE__); } }               \
+            } while(0)
 #endif
+
+#endif /* HEADER END */
+
+
